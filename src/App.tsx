f46b89982374116cb9960ab557838a4b45f12512
unknown <hamed.sh0513@gmail.com>
2024-12-02 16:32:24 +0330

    Add: Button personality

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [index, setIndex] = useState(0);
+  const array = [
+    { id: 1, title: "در تئاتر شرکت نمی کنم" },
+    { id: 2, title: "آیا مطمئنی؟" },
+    { id: 3, title: "مگر رنگ سبز فسفری مورد علاقت نبود؟ چرا پس اونو نمیزنی" },
+    { id: 4, title: "بازم مطمعنی؟ داری اشتباه می‌کنی" },
+  ];
+
+  const handleCancel = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % array.length);
+  };
+
+  const handleAccept = () => {
+    console.log("OKOK");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="container">
+      <button className="btn submit-btn" onClick={handleAccept}>
+        در تئاتر شرکت می کنم
+      </button>
+      <button className="btn cancel-btn" onClick={handleCancel}>
+        {array[index].title}
+      </button>
+    </div>
+  );
+};
 
-export default App
+export default App;
